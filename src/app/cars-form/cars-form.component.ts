30fda964d4f8f33a870c23bccc74c321823e5d44
YuriLukashenko <yuri.lukashenko@computools.com>
2020-05-25 15:05:08 +0300

    Add layout and functional of car components

---
+++ src/app/cars-form/cars-form.component.ts
@@ -0,0 +1,45 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Car} from '../car.model';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-cars-form',
+  templateUrl: './cars-form.component.html',
+  styleUrls: ['./cars-form.component.css']
+})
+export class CarsFormComponent implements OnInit {
+
+  private id = 2;
+  carName = '';
+  carModel = '';
+
+  @Output() addCar = new EventEmitter<Car>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAdd() {
+    if (this.carModel === '' || this.carName === '') {
+      return;
+    }
+
+    this.id++;
+    const car = new Car(
+      this.carName,
+      moment().format('DD.MM.YY'),
+      this.carModel,
+      false,
+      this.id
+    );
+
+    this.addCar.emit(car);
+
+    this.carModel = '';
+    this.carName = '';
+  }
+
+  onLoad() {
+  }
+}
